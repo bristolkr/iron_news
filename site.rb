85640385c96086cc97bdad885b21d0e426d67d4b
Kelly Bristol <bristolkr@gmail.com>
2015-05-19 16:31:29 -0400

    now with Slim/SASS

---
+++ site.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'tilt'
+require 'erb'
+require 'webrick'
+require 'yaml'
+
+ROOT = File.dirname(__FILE__)
+
+PORT = ENV["PORT"] || 8000
+
+server = WEBrick::HTTPServer.new(:Port => PORT)
+
+server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/assets"
+
+server.mount_proc '/' do |req, res|
+  # data = YAML.load_file('data.yml')
+  template = Tilt.new("#{ROOT}/index.slim")
+  res.body = template.render
+end
+
+server.mount_proc '/index.css' do |req, res|
+  res.body = Tilt.new("#{ROOT}/styles/index.sass").render
+end
+
+trap 'INT' do
+  server.shutdown
+end
+
+server.start
